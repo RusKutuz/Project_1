e604bf07f6f926fd9743abc799463f50495b4a29
RusKutuz <143821@mail.ru>
2020-05-29 10:41:25 +0300

    add cicles practic

--- script.js
+++ script.js
@@ -1,17 +1,33 @@
-const numberOfFilms = +prompt ('Сколько фильмов Вы уже посмотрели?', ' ');
+"use strict";
+
+let numberOfFilms = 0;
+while (numberOfFilms < 1 || numberOfFilms > 5) {
+numberOfFilms = + prompt ('Сколько фильмов Вы уже посмотрели?', ' ');
+}
+if (numberOfFilms < 2 && numberOfFilms > 0) {
+  alert ('Слишком мало фильмов');
+} else if (numberOfFilms > 1 && numberOfFilms < 4) {
+  alert ('Вы хороший зритель');
+} else if (numberOfFilms > 3 && numberOfFilms < 6) {
+  alert ('Вы киноман!');
+} else {
+  alert ('Произошла ошибка');
+}
 const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
     actors: {},
     genres: [],
     privat: false,
+};
+
+for (let i = 0; i < numberOfFilms; i++) {
+  const a = prompt("Один из последних просмотренных фильмов?", '');
+  const b = + prompt("На сколько оцените его (1-8)?", '');
+  if (a != null && a !='' && a.length < 50 & b != null && b > 0 && b < 9) {
+  personalMovieDB.movies[a] = b;
+  } else {
+    i--;
+  }
 }
-const a = prompt("Один из последних просмотренных фильмов?", '');
-    const  b = prompt("На сколько оцените его?", '');
-const      c = prompt('Один из последних просмотренных фильмов?', '');
-  const    d = prompt('На сколько оцените его?', '');
-
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
-
 console.log(personalMovieDB);
