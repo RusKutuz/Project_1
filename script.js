c7c7067bdc316af9cd14e7fc03b9e1c4cd87f698
RusKutuz <143821@mail.ru>
2020-05-29 22:59:44 +0300

    Practica 3 - Functions

--- script.js
+++ script.js
@@ -1,18 +1,27 @@
 "use strict";
 
-let numberOfFilms = 0;
-while (numberOfFilms < 1 || numberOfFilms > 5) {
-numberOfFilms = + prompt ('Сколько фильмов Вы уже посмотрели?', ' ');
+let numberOfFilms;
+function start() {
+   while (numberOfFilms < 1 || numberOfFilms > 5 || numberOfFilms == null || isNaN(numberOfFilms) || numberOfFilms === undefined) {
+  numberOfFilms = + prompt ('Сколько фильмов Вы уже посмотрели?', ' ');
 }
-if (numberOfFilms < 2 && numberOfFilms > 0) {
-  alert ('Слишком мало фильмов');
-} else if (numberOfFilms > 1 && numberOfFilms < 4) {
-  alert ('Вы хороший зритель');
-} else if (numberOfFilms > 3 && numberOfFilms < 6) {
-  alert ('Вы киноман!');
-} else {
-  alert ('Произошла ошибка');
 }
+start();
+
+function ocenkaZritelya () {
+  if (numberOfFilms < 2 && numberOfFilms > 0) {
+    alert ('Слишком мало фильмов');
+  } else if (numberOfFilms > 1 && numberOfFilms < 4) {
+    alert ('Вы хороший зритель');
+  } else if (numberOfFilms > 3 && numberOfFilms < 6) {
+    alert ('Вы киноман!');
+  } else {
+    alert ('Произошла ошибка');
+} 
+}
+ocenkaZritelya();
+
+
 const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
@@ -21,13 +30,36 @@ const personalMovieDB = {
     privat: false,
 };
 
-for (let i = 0; i < numberOfFilms; i++) {
-  const a = prompt("Один из последних просмотренных фильмов?", '');
-  const b = + prompt("На сколько оцените его (1-8)?", '');
-  if (a != null && a !='' && a.length < 50 & b != null && b > 0 && b < 9) {
-  personalMovieDB.movies[a] = b;
-  } else {
-    i--;
+function kakieFilmi() {
+  for (let i = 0; i < numberOfFilms; i++) {
+    const a = prompt("Один из последних просмотренных фильмов?", '');
+    const b = + prompt("На сколько оцените его (1-8)?", '');
+    if (a != null && a !='' && a.length < 50 & b != null && b > 0 && b < 9) {
+    personalMovieDB.movies[a] = b;
+    } else {
+      i--;
+    }
   }
 }
-console.log(personalMovieDB);
+kakieFilmi();
+
+function writeYourGenres() {
+  for (let i = 1; i <= 3; i++) {
+  let gen = prompt (`Ваш любимый жанр №${i}`,'');
+  if (gen == null || gen === undefined || gen == '') {
+    i--;
+  } else {
+    personalMovieDB.genres.push(gen);
+  }
+}
+}
+writeYourGenres();
+
+function showMyDB (hidden) {
+  if (!hidden) {
+    console.log(personalMovieDB);
+  }
+}
+showMyDB(personalMovieDB.privat);
+
+
